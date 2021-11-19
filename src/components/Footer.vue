a5158463c9ae946db7e1923de67d6523e06c9a59
gdlyeabkov <gdlyeabkov@gmail.com>
2021-11-19 22:51:03 +0300

    addheaderandfooter

---
+++ src/components/Footer.vue
@@ -0,0 +1,481 @@
+<template>
+    <div class="footer">
+        <div class="subscribeContainer">
+            <div class="subscribeContainerItem subscribeItemHeader">
+                <span class="material-icons subscribeIcon">
+                    local_post_office
+                </span>
+                <span class="subscribeHeader">
+                    Рассылка REG.RU. Лайфхаки, скидки и новости об IT
+                </span>
+            </div>
+            <div class="subscribeContainerItem">
+                <input type="text" class="form-control w-25" />
+                <button class="btn btn-info subscribeBtn">
+                    Подписаться
+                </button>
+            </div>
+            <span class="subscribeLabel">
+                Подписываясь, вы принимаете 
+                <span class="subscribeRules">
+                    правила рассылок
+                </span>
+                 на сайте REG.RU
+            </span>
+        </div>
+        <hr class="footerSeparator" />
+        <div class="footerBreadcrumbs">
+            <span class="footerBreadcrumbsItem">
+                Главная 
+            </span>
+            <span class="material-icons footerBreadcrumbsSeparator">
+                chevron_right
+            </span>
+            <span class="footerBreadcrumbsItem">
+                Dedicated серверы
+            </span>
+        </div>
+        <div class="footerLinksContainer">
+            <div class="footerLinksRow">
+                <div class="footerLinksItem">
+                    <span class="footerLinksItemHeader">
+                        REG.RU
+                    </span>
+                    <span class="footerLinksItemContent">
+                        О компании
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Контакты и офисы
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Новости
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Карьера в REG.RU
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Команда
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Отзывы клиентов
+                    </span>
+                </div>
+                <div class="footerLinksItem">
+                    <span class="footerLinksItemHeader">
+                        Услуги
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Стоимость услуг
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Хостинг
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Зарегистрировать домен
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Конструктор и CMS
+                    </span>
+                    <span class="footerLinksItemContent">
+                        REG.Site
+                    </span>
+                    <span class="footerLinksItemContent">
+                        VPS
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Серверы и ДЦ
+                    </span>
+                    <span class="footerLinksItemContent">
+                        SSL-сертификат
+                    </span>
+                </div>
+                <div class="footerLinksItem">
+                    <span class="footerLinksItemHeader">
+                        Партнёрам
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Партнёрские программы
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Профессионалам IT
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Бонусная программа
+                    </span>
+                    <span class="footerLinksItemContent">
+                        REG.API
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Промоматериалы
+                    </span>
+                </div>
+                <div class="footerLinksItem">
+                    <div class="footerBenefits">
+                        <img src="https://www.reg.ru/i/icon/footer-icons/logo.svg" alt="" />
+                        <img src="https://www.reg.ru/i/icon/footer-icons/number-one.svg" alt="" />
+                    </div>
+                    <span class="footerLinksItemHeader">
+                        +7 (495) 580-11-11
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Телефон в Москве
+                    </span>
+                    <span class="footerLinksItemHeader">
+                        8 800 555-34-78
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Бесплатный звонок по России
+                    </span>
+                    <div>
+                        <div>
+                            <a class="btn btn-primary" style="background-color: #4c75a3;" href="#!" role="button">
+                                <i class="fab fa-vk"></i>
+                            </a>
+                            <a class="btn btn-primary" style="background-color: #3b5998;" href="#!" role="button">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                            <a class="btn btn-primary" style="background-color: #55acee;" href="#!" role="button">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                            <a class="btn btn-primary" style="background-color: #ed302f;" href="#!" role="button">
+                                <i class="fab fa-youtube"></i>
+                            </a>
+                        </div>
+                        <div>
+                            <a class="btn btn-primary" style="background-color: #ac2bac;" href="#!" role="button">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                            <a class="btn btn-primary" style="background-color: #00ccff;" href="#!" role="button">
+                                <i class="fab fa-telegram"></i>
+                            </a>
+                            <a class="btn btn-primary" style="background-color: #00ccff;" href="#!" role="button">
+                                <i class="fab fa-telegram"></i>
+                            </a>
+                            <a class="btn btn-primary" style="background-color: #00ccff;" href="#!" role="button">
+                                <i class="fab fa-telegram"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <span class="footerLinksItemContent">
+                        Нашли опечатку?
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Выделите и нажмите Ctrl+Enter
+                    </span>
+                </div>
+            </div>
+            <div class="footerLinksRow">
+                <div class="footerLinksItem">
+                    <span class="footerLinksItemHeader">
+                        Полезное
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Акции и скидки
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Мероприятия
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Юридическим лицам
+                    </span>
+                    <span class="footerLinksItemContent">
+                        ЭДО
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Whois
+                    </span>
+                </div>
+                <div class="footerLinksItem">
+                    <span class="footerLinksItemHeader">
+                        Помощь
+                    </span>
+                    <span class="footerLinksItemContent">
+                        База знаний
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Вопросы и ответы
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Шаблоны заявлений
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Оставить отзыв, предложение
+                    </span>
+                    <span class="footerLinksItemContent">
+                        Сообщить о нарушении
+                    </span>
+                </div>
+                <div class="footerLinksItem">
+                    <span class="footerLinksItemHeader">
+                        
+                    </span>
+                    <span class="footerLinksItemContent">
+                        
+                    </span>
+                    <span class="footerLinksItemContent">
+                        
+                    </span>
+                    <span class="footerLinksItemContent">
+                        
+                    </span>
+                    <span class="footerLinksItemContent">
+                        
+                    </span>
+                    <span class="footerLinksItemContent">
+                        
+                    </span>
+                    <span class="footerLinksItemContent">
+                        
+                    </span>
+                </div>
+                <div class="footerLinksItem">
+                    <span class="footerLinksItemHeader">
+                        
+                    </span>
+                    <span class="footerLinksItemContent">
+                        
+                    </span>
+                    <span class="footerLinksItemContent">
+                        
+                    </span>
+                    <span class="footerLinksItemContent">
+                        
+                    </span>
+                    <span class="footerLinksItemContent">
+                        
+                    </span>
+                    <span class="footerLinksItemContent">
+                        
+                    </span>
+                    <span class="footerLinksItemContent">
+                        
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="payServices">
+            <img class="payService" src="https://www.reg.ru/i/icon/payment-methods/mastercard.svg" alt="" />
+            <img class="payService" src="https://www.reg.ru/i/icon/payment-methods/paypal.svg" alt="" />
+            <img class="payService" src="https://www.reg.ru/i/icon/payment-methods/visa.svg" alt="" />
+            <img class="payService" src="https://www.reg.ru/i/icon/payment-methods/applepay.svg" alt="" />
+            <img class="payService" src="https://www.reg.ru/i/icon/payment-methods/alfabank.svg" alt="" />
+            <img class="payService" src="https://www.reg.ru/i/icon/payment-methods/webmoney.svg" alt="" />
+            <img class="payService" src="https://www.reg.ru/i/icon/payment-methods/samsungpay.svg" alt="" />
+            <img class="payService" src="https://www.reg.ru/i/icon/payment-methods/sberbank.svg" alt="" />
+            <img class="payService" src="https://www.reg.ru/i/icon/payment-methods/qiwi.svg" alt="" />
+            <span class="othersPayMethods">
+                Все способы оплаты
+            </span>
+        </div>
+        <hr class="footerSeparator" />
+        <div class="footerInfo">
+            <div class="footerInfoItem">
+                <span class="footerInfoItemHeader">
+                    © ООО «Регистратор доменных имен РЕГ.РУ»
+                </span>
+                <span class="footerInfoItemContent">
+                    Правила пользования сайтом
+                </span>
+                <span class="footerInfoItemContent">
+                    Политика обработки персональных данных
+                </span>
+            </div>
+            <img src="https://www.reg.ru/i/icon/footer-icons/partner_logo_icann.svg" alt="" />
+            <img src="https://www.reg.ru/i/icon/footer-icons/partner_logo_cctld.svg" alt="" />
+            <button class="btn btn-info">
+                Internation version
+            </button>
+        </div>
+        <div class="footerDomains">
+            <div class="footerDomainsItem">
+                <span class="footerDomainsHeader">
+                    show.reg.ru
+                </span>
+                <span class="footerDomainsContent">
+                    Магазин доменов
+                </span>
+            </div>
+            <div class="footerDomainsItem">
+                <span class="footerDomainsHeader">
+                    statonline.ru
+                </span>
+                <span class="footerDomainsContent">
+                    Доменная статистика
+                </span>
+            </div>
+            <div class="footerDomainsItem">
+                <span class="footerDomainsHeader">
+                    reg.ru/blog
+                </span>
+                <span class="footerDomainsContent">
+                    Мы пишем
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+<style scoped>
+    
+    .footer {
+        background-color: rgb(0, 50, 100);
+        min-height: 750px;
+    }
+
+    .footerBreadcrumbs {
+        display: flex;
+        align-items: center;
+    }
+
+    .footerInfo {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .footerInfoItem {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .footerInfoItemHeader {
+        color: rgb(255, 255, 255);
+    }
+
+    .footerInfoItemContent {
+        color: rgb(0, 200, 200);
+        font-size: 12px;
+    }
+
+    .footerDomains {
+        background-color: rgb(0, 100, 150);
+        height: 100px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .footerDomainsItem {
+        display: flex;
+        flex-direction: column;
+        margin: 0px 75px;
+        color: rgb(255, 255, 255);
+    }
+
+    .footerDomainsHeader {
+        font-weight: bolder;
+        font-size: 22px;
+    }
+
+    .footerDomainsContent {
+
+    }
+
+    .payServices {
+        display: flex;
+        justify-content: center;
+    }
+
+    .payService {
+        margin: 0px 25px;
+    }
+
+    .othersPayMethods {
+        margin: 0px 25px;
+    }
+
+    .othersPayMethods {
+        color: rgb(0, 250, 255);
+    }
+
+    .footerLinksContainer {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+    }
+
+    .footerLinksRow {
+        display: flex;
+        margin: 50px 0px;
+    }
+
+    .footerLinksItem {
+        width: 235px;
+        display: flex;
+        flex-direction: column;
+        color: rgb(255, 255, 255);
+        margin: 0px 25px;
+    }
+
+    .footerLinksItemHeader {
+        font-weight: bolder;
+        font-size: 24px;
+    }
+
+    .footerBreadcrumbsSeparator {
+        color: rgb(0, 100, 200);
+        font-size: 14px;
+        margin: 0px 15px;
+    }
+
+    .footerBreadcrumbsItem {
+        color: rgb(100, 150, 200);
+    }
+
+    .footerSeparator {
+        background-color: rgb(100, 150, 200);
+    }
+
+    .subscribeBtn {
+        color: rgb(255, 255, 255);
+        font-weight: bolder;
+        width: 150px;
+        margin-left: 5px;
+    }
+
+    .subscribeContainer {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .subscribeContainerItem {
+        display: flex;
+    }
+
+    .subscribeRules {
+        color: rgb(0, 100, 255);
+    }
+
+    .subscribeLabel {
+        color: rgb(255, 255, 255);
+    }
+
+    .subscribeIcon {
+        color: rgb(255, 0, 0);
+        font-size: 36px;
+    }
+
+    .subscribeHeader {
+        color: rgb(255, 255, 255);
+        font-size: 24px;
+        margin-left: 50px;
+    }
+
+    .subscribeItemHeader {
+        height: 75px;
+        width: 495px;
+        align-items: center;
+    }
+
+    .footerBenefits {
+        display: flex;
+    }
+
+    
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="header">
+        <div class="headerTop">
+            <div class="headerItem">
+                <img class="headerItemElement" src="https://www.reg.ru/i/svg/b-header__logo_site_ru.svg" alt="">
+            </div>
+            <div class="headerItem">
+                <div class="input-group inputRailwayDatePicker">
+                    <input v-model="domain" type="text" placeholder="Введите домен или слово" class="form-control w-50" />
+                    <span  class="input-group-text btn btn-success" id="basic-addon1">Подобрать</span>
+                    <span  class="input-group-text btn btn-dark" id="basic-addon1">Whois</span>
+                </div>
+            </div>
+            <div class="headerItem">
+                <span class="headerItemElement">
+                    Регистрация
+                </span>
+                <button class="btn btn-info headerItemElement loginBtn">
+                    Вход
+                </button>
+            </div>
+        </div>
+        <div class="headerBottom">
+            <span class="material-icons headerBottomItem">
+                menu
+            </span>
+            <span class="headerBottomItem">
+                Домены
+            </span>
+            <span class="headerBottomItem">
+                Конструктор и CMS
+            </span>
+            <span class="headerBottomItem">
+                Хостинг
+            </span>
+            <span class="headerBottomItem">
+                VPS
+            </span>
+            <span class="headerBottomItem">
+                Серверы и ДЦ
+            </span>
+            <span class="headerBottomItem">
+                SSL
+            </span>
+            <div class="headerBottomItem">
+                <span>
+                    Сервисы
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    
+    .headerTop {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: rgb(250, 250, 220);
+        box-sizing: border-box;
+        /* padding: 0px 30px; */
+    }
+
+    .headerItem {
+        margin: 0px 25px;
+    }
+
+    .headerItemElement {
+        margin: 0px 15px;
+    }
+
+    .loginBtn {
+        font-weight: bolder;
+        color: rgb(255, 255, 255);
+    }
+
+    .headerBottom {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 50px;
+        background-color: rgb(0, 100, 200);
+        color: rgb(255, 255, 255);
+        font-weight: bolder;
+    }
+
+    .headerBottomItem {
+        margin: 0px 15px;
+        height: 100%;
+        padding: 0px 15px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        cursor: pointer;
+    }
+
+    .headerBottomItem:hover {
+        background-color: rgb(0, 150, 250);
+    }
+
+</style>
